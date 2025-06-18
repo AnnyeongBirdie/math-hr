a8432b49a2c09219cc2484116ef330c72bfa8cee
Kah-ul Kim <kahul.kim@gmail.com>
2025-06-18 15:50:54 +0900

    Fix package.json setup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/math-hr/', // 👈 this tells GitHub Pages where to find your app
+  base: '/math-hr/',
   plugins: [react()],
 });
